2e4c68329d5cc62de2b55442a12e7f2129f9297c
kaleab-mureja <kaleabmureja123@gmail.com>
2025-07-23 21:02:53 +0300

    a little update

--- auth/routes/api.php
+++ auth/routes/api.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use Illuminate\Http\Request;
@@ -10,4 +11,5 @@ Route::post('login', [AuthController::class, 'login']);
 Route::group(['middleware' => ['auth:api']], function () {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
+	Route::get('me', [AuthController::class, 'me']);
 });
